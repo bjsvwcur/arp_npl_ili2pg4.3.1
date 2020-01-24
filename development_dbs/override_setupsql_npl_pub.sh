ee02a2e8c1a6441f315723d7f043a3a2bac7653f
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-01-24 10:49:27 +0100

    Add files via upload

---
+++ development_dbs/override_setupsql_npl_pub.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -f /pgconf/setup.sql /pgconf/grants.sql
+curl https://raw.githubusercontent.com/sogis/oereb-db/master/sql/setup_original.sql > /pgconf/setup_original.sql
+curl https://raw.githubusercontent.com/sogis/oereb-db/master/sql/set_role.sql > /pgconf/set_role.sql
+cat /pgconf/setup_original.sql /pgconf/set_role.sql > /pgconf/setup.sql
+# Hack/unnecessary but required command (PG_WRITE_USER does not exist yet):
+sed -i s/:PG_WRITE_USER/${PG_USER}/g /pgconf/setup.sql
+
+echo "CREATE USER :PG_WRITE_USER WITH PASSWORD :'PG_WRITE_PASSWORD'; GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA arp_npl_pub TO :PG_WRITE_USER; GRANT USAGE ON ALL SEQUENCES IN SCHEMA arp_npl_pub TO :PG_WRITE_USER;" > /pgconf/grants.sql
+
+exec "$@"
